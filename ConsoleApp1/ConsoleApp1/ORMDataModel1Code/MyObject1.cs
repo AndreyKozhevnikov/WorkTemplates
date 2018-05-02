4c6bbcb8d8a5da0e0b29eeffffab4c780158b18b
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-05-02 12:48:20 +0300

    add xpo example

---
+++ ConsoleApp1/ConsoleApp1/ORMDataModel1Code/MyObject1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace ConsoleApp1.dxT631023 {
+
+    public partial class Contact {
+        public Contact(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1.dxT631023;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            ConnectionHelper.Connect(DevExpress.Xpo.DB.AutoCreateOption.DatabaseAndSchema);
+            var session = new Session();
+        }
+    }
+}
